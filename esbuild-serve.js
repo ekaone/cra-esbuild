e3f344195eb1846b27accdc1b5e36286740d6fee
ekaone <ekaone3033@gmail.com>
2022-12-22 16:08:38 +0700

    init

---
+++ esbuild-serve.js
@@ -0,0 +1,20 @@
+const esbuild = require("esbuild");
+const inlineImage = require("esbuild-plugin-inline-image");
+
+esbuild
+  .serve(
+    {
+      servedir: "public",
+      port: 3000,
+    },
+    {
+      entryPoints: ["./src/index.js"],
+      outfile: "./public/dist/app.js",
+      bundle: true,
+      loader: {
+        ".js": "jsx",
+      },
+      plugins: [inlineImage()],
+    }
+  )
+  .catch(() => process.exit(1));
